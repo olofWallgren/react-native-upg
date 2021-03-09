c709d023b62602dde53dcbdd26aaee04baf4ac14
Olof-Wallgren <naturel.olof@hotmail.com>
2021-03-09 21:28:47 +0100

    testing

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hejsvejs</Text>
+      <Text>hejsvejs1</Text>
       <StatusBar style="auto" />
     </View>
   );
